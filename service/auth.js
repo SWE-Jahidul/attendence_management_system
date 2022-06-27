fbdb88d0b8af76b769777b0e1f1fcd7a2a6c0ba3
jahidul <programmerjahid162@gmail.com>
2022-06-27 20:29:13 +0600

    adding user

--- service/auth.js
+++ service/auth.js
@@ -1,41 +1,44 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
-const User = require('../models/User');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { findUserByProperty, createNewUser } = require("./user");
 
-const registerService = async() =>{
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ message: "User Alredy Exist " });
-    }
+const registerService = async ({ name, email, password }) => {
+  let user = await findUserByProperty("email", email);
 
-    user = new User({ name, email, password, accountStatus });
+  if (user) {
+    const error = new Error("User Alredy Exist");
+    error.status = 400;
+    throw error;
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+   console.log( hash);
+  return createNewUser({name, email, password: hash});
+};
 
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    user.password = hash;
-    await user.save();
-}
 
-const loginService = async () =>{
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({
-        message: "Invalid Credential",
-      });
-    }
+const loginService = async () => {
+  // const user = await User.findOne({ email });
+  // if (!user) {
+  //   return res.status(400).json({
+  //     message: "Invalid Credential",
+  //   });
+  // }
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid Credential !" });
-    }
+  // const isMatch = await bcrypt.compare(password, user.password);
+  // if (!isMatch) {
+  //   return res.status(400).json({ message: "Invalid Credential !" });
+  // }
 
-    delete user._doc.password;
+  // delete user._doc.password;
 
-    const token = jwt.sign(user._doc, "secret-key", {
-      expiresIn: "30s",
-    });
-}
+  // const token = jwt.sign(user._doc, "secret-key", {
+  //   expiresIn: "30s",
+  // });
+};
+
 module.exports = {
-    loginService,
-    registerService
-}
+  loginService,
+  registerService,
+};
